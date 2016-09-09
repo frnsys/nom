c4495dc1b2b23199473423b9b797f491db1cdb8e
Francis Tseng <f+accounts@frnsys.com>
2016-09-08 22:40:25 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nom',
-    version='1.1.2',
+    version='1.1.3',
     description='tool for managing markdown notes',
     url='https://github.com/frnsys/nom',
     author='Francis Tseng',
