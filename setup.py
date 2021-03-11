bd0b24bedbf64ff236d206f90b4c674ec0013619
Francis Tseng <f+accounts@frnsys.com>
2021-03-10 19:31:32 -0500

    lxml update

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'click==6.2',
         'gfm==0.0.3',
         'html2text==2015.11.4',
-        'lxml==3.5.0',
+        'lxml==4.5.0',
         'py-gfm==0.1.1',
         'watchdog==0.8.3',
         'Pygments==2.1.3',
